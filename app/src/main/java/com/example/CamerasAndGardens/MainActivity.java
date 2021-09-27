ef665be7f1e66ce58e499261e88a541c5464e242
Akos Becsey <becsey.a@gmail.com>
2021-09-27 19:46:20 +0200

    XML parsing - Image URLs

--- app/src/main/java/com/example/CamerasAndGardens/MainActivity.java
+++ app/src/main/java/com/example/CamerasAndGardens/MainActivity.java
@@ -8,6 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,11 +35,38 @@ public class MainActivity extends AppCompatActivity {
         myRecycleView.setLayoutManager(new LinearLayoutManager(this));
     }
 
+    private void parseXml() {
+        try {
+            XmlPullParser parser = XmlPullParserFactory.newInstance().newPullParser();
+            InputStream is = getAssets().open("CCTV.kml");
+
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(is, null);
+
+            int eventType = parser.getEventType(); // current event state of the parser
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if (eventType == XmlPullParser.START_TAG) {
+                    String elementName = parser.getName(); // name of the current element
+
+                    if (elementName.equals("description")) {
+                        listOfItems.add(new Item(getCameraUrlFromDescription(parser.nextText())));
+                    }
+                }
+                eventType = parser.next(); // Get next parsing event
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Error:" + e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    static private String getCameraUrlFromDescription(String description){
+        description = description.substring(description.indexOf("http:")); // from the beginning of “http:”...
+        return description.substring(0, description.indexOf(".jpg") + 4); // ...to the end of “.jpg”
+    }
+
     public void onBtnCameras(View view) {
         listOfItems.clear();
-        listOfItems.add(new Item("A"));
-        listOfItems.add(new Item("B"));
-        listOfItems.add(new Item("C"));
+        parseXml();
         myAdapter.notifyDataSetChanged();
     }
 }
